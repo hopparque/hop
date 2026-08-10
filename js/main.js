af54c51e5f7dbae10dd0a81ab960ab76c52b280c
hopparque <hoparque@gmail.com>
2026-08-10 09:54:29 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
+// js/main.js
+document.addEventListener("DOMContentLoaded", () => {
+    // Carregar Header
+    fetch('/components/header.html')
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById('header-placeholder').innerHTML = data;
+        });
 
+    // Carregar Footer
+    fetch('/components/footer.html')
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById('footer-placeholder').innerHTML = data;
+        });
+});
+
+// Função para abrir Pop-up da 3cket
+function open3cketModal(eventUrl) {
+    const modal = document.getElementById('ticket-modal');
+    const iframe = document.getElementById('ticket-iframe');
+    iframe.src = eventUrl;
+    modal.style.display = 'flex';
+}
+
+function close3cketModal() {
+    const modal = document.getElementById('ticket-modal');
+    const iframe = document.getElementById('ticket-iframe');
+    iframe.src = '';
+    modal.style.display = 'none';
+}
